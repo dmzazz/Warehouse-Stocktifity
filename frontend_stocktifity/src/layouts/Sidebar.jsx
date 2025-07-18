d4402c14c2e2c966b62de40975d9595b75324040
Dimas Azizir <dimaslaylaa@gmail.com>
2025-07-18 17:33:12 +0700

    update env url

--- frontend_stocktifity/src/layouts/Sidebar.jsx
+++ frontend_stocktifity/src/layouts/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
 
   const Logout = async () => {
     try {
-      await axios.delete("process.env.REACT_APP_API_URL/logout");
+      await axios.delete(`${process.env.REACT_APP_API_URL}/logout`);
       swal({
         title: "Are you sure?",
         text: "You want to Logout?",
